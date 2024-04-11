dfa48c9d6e2a5f4d54f8665dfc20a1e9c2947631
Charishma Myneni <Charishma.Myneni@Dell.com>
2024-04-11 15:42:49 -0500

    added testing folders

--- psve-tco-dev/test_azure_sizing/test_azure_page_sizer_connector.py
+++ psve-tco-dev/test_azure_sizing/test_azure_page_sizer_connector.py
@@ -13,6 +13,6 @@ def test_SizeSolutionFromPage2():
 
     assert SizeSolutionFromPage("OneFS 9.9","us-east-1","Archive",1.0,180,"+2d:1n",3000,1000,"Azure") == [{'Message': 1, 'Detail': 'OneFS version is not supported by sizing tool yet.'}]
 
-def test_SizeSolutionFromPage4():
+def test_SizeSolutionFromPage3():
  
   assert SizeSolutionFromPage("OneFS 9.8","us-east-1","Archive",1.0,180,"+2d:1n",3000,1000,"Azure") == [{'Message': 1, 'Detail': 'Sizing for Archive workload is not supported by sizing tool yet.'}]
